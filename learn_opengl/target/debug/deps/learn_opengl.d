830a495b0f072dada492f9d46676b0b59e33717e
Grace Todd <toddgr@oregonstate.edu>
2022-10-03 20:21:07 -0700

    build cargo

---
+++ learn_opengl/target/debug/deps/learn_opengl.d
@@ -0,0 +1,5 @@
+C:\Users\Grace\Desktop\Rust-OpenGL-Texture-Loader\learn_opengl\target\debug\deps\learn_opengl.exe: src\main.rs
+
+C:\Users\Grace\Desktop\Rust-OpenGL-Texture-Loader\learn_opengl\target\debug\deps\learn_opengl.d: src\main.rs
+
+src\main.rs:
---
+++ learn_opengl/target/debug/learn_opengl.d
@@ -0,0 +1,1 @@
+C:\Users\Grace\Desktop\Rust-OpenGL-Texture-Loader\learn_opengl\target\debug\learn_opengl.exe: C:\Users\Grace\Desktop\Rust-OpenGL-Texture-Loader\learn_opengl\src\main.rs
